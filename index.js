3f9371198c62b440cf8aad01f887a113dac56c51
Pushpaje Kumar <97593143+Pushpajekumar@users.noreply.github.com>
2022-11-26 18:36:29 +0000

    First route created

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello to codespaes")
+const app = require("./main");
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at Port ${PORT} ...`);
+});
---
+++ main.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(<h1>Server is running</h1>);
+});
+
+module.exports = app;
